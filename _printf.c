b50c63689a3fe2eee55e13663dbb10922d618b9a
Chany-1 <chanywang9@gmail.com>
2023-09-19 11:01:43 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int count = 0; // To keep track of the number of characters printed
+
+    for (int i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            putchar(format[i]);
+            count++;
+        }
+        else
+        {
+            i++; // Move past the '%'
+
